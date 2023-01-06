83b34ac4aeee87d1de35d4b33037a7d5fd303686
Gabriel Barreto <gh.barreto@hotmail.com>
2023-01-05 20:55:12 -0800

    added env

--- utils/fetch_posts.ts
+++ utils/fetch_posts.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchPosts = async () => {
     try {
-        const { data } = await axios.get(`http://localhost:3000/api/fetch_posts`);
+        const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_WEBSITE}/api/fetch_posts`);
         return { posts: data };
     } catch (err) {
         return {
